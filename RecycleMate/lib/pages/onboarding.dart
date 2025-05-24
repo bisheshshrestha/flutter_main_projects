d4e9183c5b8ff63706acf74ec58b9c980b15b9fa
bisehshshrestha <bisheshshrestha652@gmail.com>
2025-05-24 09:59:47 +0545

    onboarding page completed

--- RecycleMate/lib/pages/onboarding.dart
+++ RecycleMate/lib/pages/onboarding.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:recycle_mate/services/widget_support.dart';
 
 class Onboarding extends StatefulWidget {
   const Onboarding({super.key});
@@ -14,7 +15,35 @@ class _OnboardingState extends State<Onboarding> {
       body: Container(
         child: Column(
           children: [
-            Image.asset("assets/images/onboard.png")
+            SizedBox(height: 50,),
+            Image.asset("assets/images/onboard.png"),
+            SizedBox(height: 50.0),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0),
+              child: Text(
+                "Recycle your waste products!",
+                style: AppWidget.headlineTextStyle(32.0),
+              ),
+            ),
+            SizedBox(height: 30,),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+              child: Text("Easily collect household waste and generate less waste",style: AppWidget.normalTextStyle(20.0),),
+            ),
+            SizedBox(height:50,),
+            Material(
+              elevation: 5.0,
+              borderRadius: BorderRadius.circular(40),
+              child: Container(
+                height: 70,
+                width: MediaQuery.of(context).size.width / 1.5,
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(40),
+                ),
+                child: Center(child: Text("Get Started",style: AppWidget.whiteTextStyle(24.0),)),
+              ),
+            )
           ],
         ),
       ),
---
+++ RecycleMate/lib/services/widget_support.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class AppWidget{
+  static TextStyle headlineTextStyle(double size){
+    return TextStyle(
+      color: Colors.black,
+      fontSize: size,
+      fontWeight: FontWeight.bold
+    );
+  }
+
+  static TextStyle normalTextStyle(double size){
+    return TextStyle(
+        color: Colors.black,
+        fontSize: size,
+        fontWeight: FontWeight.w500
+    );
+  }
+  static TextStyle whiteTextStyle(double size){
+    return TextStyle(
+        color: Colors.white,
+        fontSize: size,
+        fontWeight: FontWeight.bold
+    );
+  }
+}
